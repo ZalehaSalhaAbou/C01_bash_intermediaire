1a9d7ed52f0c3a47549f62261e1da0f62b888e69
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-15 00:18:20 +0100

    Update and rename count_file.sh to count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Script pour compter le nombre de fichiers dans un répertoire
-
 echo "Entrez le nom du répertoire :"
 read repertoire
-
 # Vérifie si le répertoire existe
 if [ -d "$repertoire" ]; then
     count=$(ls -p "$repertoire" | grep -v / | wc -l)
