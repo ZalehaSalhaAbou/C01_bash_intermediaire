6a498924af9aa1ff36e9593ee4905498605d80cd
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-16 12:41:27 +0100

    Update and rename show_disk_usage.sh to simple_calculator.sh

--- show_disk_usage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Ce script affiche l'utilisation du disque
-
-echo "Utilisation globale du disque :"
-df -h
-
-echo
-echo "Utilisation détaillée dans le dossier courant :"
-du -sh ./* 2>/dev/null
