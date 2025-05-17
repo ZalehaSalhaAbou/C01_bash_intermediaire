3f69f80e1819bcd309a4c87faec34c7156e00af5
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-05-17 23:48:53 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -2,11 +2,11 @@
 # Compte le nombre de fichiers dans un dossier
 
 echo "Entrez le nom du dossier :"
-read folder
+read filename
 
-if [ -d "$folder" ]; then
-    count=$(ls "$folder" | wc -l)
-    echo "Le dossier '$folder' contient $count fichier(s)."
+if [ -d "$filename" ]; then
+    count=$(ls "$filename" | wc -l)
+    echo "Le dossier '$filename' contient $count fichier(s)."
 else
-    echo "Le dossier '$folder' n'existe pas."
+    echo "Le dossier '$filename' n'existe pas."
 fi
