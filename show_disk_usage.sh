54c92afcb30f6d0466fb083bcb08685d705b4710
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-06-02 15:17:26 +0100

    Update show_disk_usage.sh

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-# Affiche l'utilisation du disque
-
+# Affiche l'utilisation du disquedisque
+echo "Résumé de l'utilisation du disque (toutes les partitions) :"
 df -h
+
+echo ""
+read -p "Voulez-vous voir l'utilisation détaillée d'un dossier spécifique ? (o/n) : " rep
+
+if [[ "$rep" =~ ^[oOyY]$ ]]; then
+  read -p "Entrez le chemin du dossier : " dossier
+  if [ ! -d "$dossier" ]; then
+    echo "Erreur : $dossier n'est pas un dossier."
+    exit 2
+  fi
+  du -sh "$dossier"
+fi
