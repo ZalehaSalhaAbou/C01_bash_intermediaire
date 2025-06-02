8c2846e0e41c5abcb6afba3b6c05af9e417ca53e
ZalehaSalhaAbou <salhaabouz@gmail.com>
2025-06-02 15:15:20 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Affiche chaque argument passé en ligne de commande
+if [ $# -eq 0 ]; then
+  echo "Utilisation : $0 <arg1> <arg2> ..."
+  exit 1
+fi
 
 i=1
-for arg in "$@"
-do
-    echo "Argument $i : $arg"
-    i=$((i + 1))
+for arg in "$@"; do
+  echo "Argument $i: $arg"
+  i=$((i+1))
 done
